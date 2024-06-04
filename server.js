732d2dbd857bf0cbfaaf87877a23c5131ecb1c35
Al Amin Sheikh <azalamin@Als-MacBook-Pro.local>
2024-06-04 12:10:11 +0600

    valorant sens added

--- server.js
+++ server.js
@@ -59,7 +59,6 @@ app.get('/calculateValue', async (req, res) => {
     }
 });
 
-
 app.get('/fetchGameNames', async (req, res) => {
     try {
         const { gameName } = req.query;
@@ -71,20 +70,23 @@ app.get('/fetchGameNames', async (req, res) => {
         }
         const data = await response.json();
         res.json(data);
-        console.log(data)
+        // console.log(data)
     } catch (error) {
         console.error('Error:', error);
         res.status(500).json({ error: 'Failed to verify game name. Please try again.' });
     }
 });
 
-const calculateSensitivity = (overWatchSens, aimPreference, dpi) => {
-    const valorantSens = overWatchSens * 0.07;
+const calculateSensitivity = (calculatedSens, aimPreference, dpi) => {
+    const valorantSens = Number(calculatedSens); // Ensure calculatedSens is a number
+    // console.log(`Valorant Sensitivity: ${valorantSens}`);
     let feedback;
 
     // Ensure we are working with a fixed decimal point value
     let focusValue = valorantSens.toFixed(3);
     let decimalPart = focusValue.split('.')[1]; // Get the decimal part of the sensitivity
+
+    // console.log(`Decimal Part: ${decimalPart}`);
 
     if (!decimalPart) {
         decimalPart = '000'; // Default to '000' if there's no decimal part
@@ -93,16 +95,16 @@ const calculateSensitivity = (overWatchSens, aimPreference, dpi) => {
     let selectedDigit;
     switch (aimPreference) {
         case 'arm':
-            selectedDigit = decimalPart[0] || '0';
+            selectedDigit = decimalPart[0] || '0'; // First digit after decimal
             break;
         case 'wrist':
-            selectedDigit = decimalPart[1] || '0';
+            selectedDigit = decimalPart[1] || '0'; // Second digit after decimal
             break;
         case 'finger':
-            selectedDigit = decimalPart[2] || '0';
+            selectedDigit = decimalPart[2] || '0'; // Third digit after decimal
             break;
         default:
-            selectedDigit = decimalPart[0] || '0';
+            selectedDigit = decimalPart[0] || '0'; // Default to first digit
             break;
     }
 
@@ -123,131 +125,60 @@ const calculateSensitivity = (overWatchSens, aimPreference, dpi) => {
         9: '15 minutes'
     };
 
-    switch (selectedDigit) {
-        case 0:
-            feedback = {
-                pros: 'Neutral/flat grip for balance. The aiming preference you selected may be your "anchor point". You can either select a new aiming preference or senstailor will tailor a new sensitivity based on your current aiming preference.',
-                cons: '',
-                orientation: orientationMapping[selectedDigit],
-                clockImageUrl: `/public/clock_images/${selectedDigit}.png`,
-                newSensitivity: valorantSens,
-                dpi: dpi,
-                aimPreference: aimPreference,
-            };
-            break;
-        case 1:
-            feedback = {
-                pros: 'This sensitivity will have the fastest general aim and fastest reaction times as well as fastest precision.',
-                cons: 'This sensitivity will be bad for tracking and you may undershoot at times. It also will be biased to left-hand side targets/shots and weaker at hitting right-hand side targets/shots.',
-                orientation: orientationMapping[selectedDigit],
-                clockImageUrl: `/public/clock_images/${selectedDigit}.png`,
-                newSensitivity: valorantSens,
-                dpi: dpi,
-                aimPreference: aimPreference,
-            };
-            break;
-        case 2:
-            feedback = {
-                pros: 'This sensitivity will have a balance between control, highest precision and even target selection on left and right.',
-                cons: 'This sensitivity has a low "range"/"field of aim" so it will undershoot and struggle with tracking.',
-                orientation: orientationMapping[selectedDigit],
-                clockImageUrl: `/public/clock_images/${selectedDigit}.png`,
-                newSensitivity: valorantSens,
-                dpi: dpi,
-                aimPreference: aimPreference,
-            };
-            break;
-        case 3:
-            feedback = {
-                pros: 'This sensitivity will have the best mouse control. You will feel a strong grip and be able to make high accuracy adjustments and 180s.',
-                cons: 'This sensitivity will be bad for tracking and will undershoot. It also will be biased to left-hand side targets/shots and weaker at hitting right-hand side targets/shots.',
-                orientation: orientationMapping[selectedDigit],
-                clockImageUrl: `/public/clock_images/${selectedDigit}.png`,
-                newSensitivity: valorantSens,
-                dpi: dpi,
-                aimPreference: aimPreference,
-            };
-            break;
-        case 4:
-            feedback = {
-                pros: 'This sensitivity is great for flick aim and spray transfers.',
-                cons: 'This sensitivity will look shaky and can be very inconsistent.',
-                orientation: orientationMapping[selectedDigit],
-                clockImageUrl: `/public/clock_images/${selectedDigit}.png`,
-                newSensitivity: valorantSens,
-                dpi: dpi,
-                aimPreference: aimPreference,
-            };
-            break;
-        case 5:
-            feedback = {
-                pros: 'This sensitivity has the best all-round aim. It\'s the most consistent for tracking, flicking, and precision aim. It also has even target selection on left and right.',
-                cons: 'This sensitivity has a very high skill ceiling to master and get desired effects. It can look very shaky.',
-                orientation: orientationMapping[selectedDigit],
-                clockImageUrl: `/public/clock_images/${selectedDigit}.png`,
-                newSensitivity: valorantSens,
-                dpi: dpi,
-                aimPreference: aimPreference,
-            };
-            break;
-        case 6:
-            feedback = {
-                pros: 'This sensitivity will have the highest first shot accuracy and tracking.',
-                cons: 'This sensitivity will feel extremely shaky and inconsistent.',
-                orientation: orientationMapping[selectedDigit],
-                clockImageUrl: `/public/clock_images/${selectedDigit}.png`,
-                newSensitivity: valorantSens,
-                dpi: dpi,
-                aimPreference: aimPreference,
-            };
-            break;
-        case 7:
-            feedback = {
-                pros: 'The sensitivity has great focus/looks stable and is great for movement and rhythm-based aiming. It also has a range/"field of aim".',
-                cons: 'This sensitivity may cause you to overshoot.',
-                orientation: orientationMapping[selectedDigit],
-                clockImageUrl: `/public/clock_images/${selectedDigit}.png`,
-                newSensitivity: valorantSens,
-                dpi: dpi,
-                aimPreference: aimPreference,
-            };
-            break;
-        case 8:
-            feedback = {
-                pros: 'The sensitivity will have "pencil aim". It\'s amazing for prefiring, adjustments, tracking, flicking and has very high mouse control. It also has even target selection on left and right.',
-                cons: 'This sensitivity will cause you to overshoot. It can feel very shaky and inconsistent and the movement can be sloppy at times.',
-                orientation: orientationMapping[selectedDigit],
-                clockImageUrl: `/public/clock_images/${selectedDigit}.png`,
-                newSensitivity: valorantSens,
-                dpi: dpi,
-                aimPreference: aimPreference,
-            };
-            break;
-        case 9:
-            feedback = {
-                pros: 'This sensitivity has the largest field of aim. It\'s very fast for adjustments and reaction time. It can also be great for movement and prefiring.',
-                cons: 'This sensitivity will be bad for micro-adjustments/precision. At times it will feel inconsistent and shaky and overshoot a lot.',
-                orientation: orientationMapping[selectedDigit],
-                clockImageUrl: `/public/clock_images/${selectedDigit}.png`,
-                newSensitivity: valorantSens,
-                dpi: dpi,
-                aimPreference: aimPreference,
-            };
-            break;
-        default:
-            feedback = {
-                pros: 'Invalid sensitivity feedback.',
-                cons: '',
-                orientation: '',
-                clockImageUrl: '',
-                newSensitivity: valorantSens,
-                dpi: dpi,
-                aimPreference: aimPreference,
-            };
-            break;
-    }
+    const feedbackMapping = {
+        0: {
+            pros: 'Neutral/flat grip for balance. The aiming preference you selected may be your "anchor point". You can either select a new aiming preference or senstailor will tailor a new sensitivity based on your current aiming preference.',
+            cons: '',
+        },
+        1: {
+            pros: 'This sensitivity will have the fastest general aim and fastest reaction times as well as fastest precision.',
+            cons: 'This sensitivity will be bad for tracking and you may undershoot at times. It also will be biased to left-hand side targets/shots and weaker at hitting right-hand side targets/shots.',
+        },
+        2: {
+            pros: 'This sensitivity will have a balance between control, highest precision and even target selection on left and right.',
+            cons: 'This sensitivity has a low "range"/"field of aim" so it will undershoot and struggle with tracking.',
+        },
+        3: {
+            pros: 'This sensitivity will have the best mouse control. You will feel a strong grip and be able to make high accuracy adjustments and 180s.',
+            cons: 'This sensitivity will be bad for tracking and will undershoot. It also will be biased to left-hand side targets/shots and weaker at hitting right-hand side targets/shots.',
+        },
+        4: {
+            pros: 'This sensitivity is great for flick aim and spray transfers.',
+            cons: 'This sensitivity will look shaky and can be very inconsistent.',
+        },
+        5: {
+            pros: 'This sensitivity has the best all-round aim. It\'s the most consistent for tracking, flicking, and precision aim. It also has even target selection on left and right.',
+            cons: 'This sensitivity has a very high skill ceiling to master and get desired effects. It can look very shaky.',
+        },
+        6: {
+            pros: 'This sensitivity will have the highest first shot accuracy and tracking.',
+            cons: 'This sensitivity will feel extremely shaky and inconsistent.',
+        },
+        7: {
+            pros: 'The sensitivity has great focus/looks stable and is great for movement and rhythm-based aiming. It also has a range/"field of aim".',
+            cons: 'This sensitivity may cause you to overshoot.',
+        },
+        8: {
+            pros: 'The sensitivity will have "pencil aim". It\'s amazing for prefiring, adjustments, tracking, flicking and has very high mouse control. It also has even target selection on left and right.',
+            cons: 'This sensitivity will cause you to overshoot. It can feel very shaky and inconsistent and the movement can be sloppy at times.',
+        },
+        9: {
+            pros: 'This sensitivity has the largest field of aim. It\'s very fast for adjustments and reaction time. It can also be great for movement and prefiring.',
+            cons: 'This sensitivity will be bad for micro-adjustments/precision. At times it will feel inconsistent and shaky and overshoot a lot.',
+        }
+    };
 
-    console.log('Generated feedback:', feedback); // Log the feedback
+    feedback = {
+        pros: feedbackMapping[selectedDigit].pros,
+        cons: feedbackMapping[selectedDigit].cons,
+        orientation: orientationMapping[selectedDigit],
+        clockImageUrl: `/public/clock_images/${selectedDigit}.png`,
+        newSensitivity: valorantSens,
+        dpi: dpi,
+        aimPreference: aimPreference,
+    };
+
+    // console.log('Generated feedback:', feedback); // Log the feedback
 
     return {
         valorantSens: valorantSens.toFixed(3),
@@ -256,12 +187,12 @@ const calculateSensitivity = (overWatchSens, aimPreference, dpi) => {
 };
 
 app.post('/convertSensitivity', (req, res) => {
-    const { overWatchSens, aimPreference, dpi } = req.body;
+    const { calculatedSens, aimPreference, dpi } = req.body;
     try {
-        const result = calculateSensitivity(overWatchSens, aimPreference, dpi);
+        const result = calculateSensitivity(calculatedSens, aimPreference, dpi);
         const { valorantSens, feedback } = result;
 
-        console.log('Returning result:', { valorantSens, feedback }); // Log the result being returned
+        // console.log('Returning result:', { valorantSens, feedback }); // Log the result being returned
 
         res.json({
             valorantSens: valorantSens,
@@ -307,10 +238,10 @@ app.get('/generateZigzag', (req, res) => {
         const { width, height, sensitivity } = req.query;
 
         // Log the input parameters
-        console.log(`Received parameters - Width: ${width}, Height: ${height}, Sensitivity: ${sensitivity}`);
+        // console.log(`Received parameters - Width: ${width}, Height: ${height}, Sensitivity: ${sensitivity}`);
 
         const amplitude = parseFloat(sensitivity) * 10; // Example transformation
-        console.log(`Calculated amplitude: ${amplitude}`);
+
         const selectedDigit = parseInt(sensitivity.split('.')[1][0], 10); // Assuming we use the first digit after decimal
         const orientationMapping = {
             0: 48,
@@ -325,7 +256,6 @@ app.get('/generateZigzag', (req, res) => {
             9: 15
         };
         const orientation = orientationMapping[selectedDigit];
-        console.log(`Using orientation: ${orientation} minutes`);
 
         if (isNaN(amplitude) || isNaN(width) || isNaN(height) || isNaN(orientation)) {
             throw new Error('Invalid input parameters');
@@ -345,7 +275,6 @@ app.get('/generateZigzag', (req, res) => {
 // OpenAI GPT-TURBO-3.5 
 app.post('/generateMessage', async (req, res) => {
     const { prompt } = req.body;
-    console.log(prompt);
     try {
         const completion = await openai.chat.completions.create({
             messages: [{ role: "system", content: prompt }],
